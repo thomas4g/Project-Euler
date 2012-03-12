fd939337cbf1d99c08e00d29afa751815567662c
Thomas Shields <me@thomasshields.net>
2012-03-12 19:34:04 -0400

    optimized 11

--- problem11.js
+++ problem11.js
@@ -10,21 +10,13 @@ function solve(grid) {
 	n = Math.sqrt(nums.length);
 	for(;i<nums.length;i++) {
 		var r = nums[i]*nums[i+1]*nums[i+2]*nums[i+3],
-		l = nums[i]*nums[i+1]*nums[i-2]*nums[i-3],
 		up = nums[i]*nums[i-n]*nums[i-2*n]*nums[i-3*n],
-		dwn = nums[i]*nums[i+n]*nums[i+2*n]*nums[i+3*n],
 		ur = nums[i]*nums[i-(n-1)]*nums[i-2*(n-1)]*nums[i-3*(n-1)],
-		ul = nums[i]*nums[i-(n+1)]*nums[i-2*(n+1)]*nums[i-3*(n+1)],
-		dr = nums[i]*nums[i+(n+1)]*nums[i+2*(n+1)]*nums[i+3*(n+1)],
-		dl = nums[i]*nums[i+(n-1)]*nums[i+2*(n-1)]*nums[i+3*(n-1)];
+		dr = nums[i]*nums[i+(n+1)]*nums[i+2*(n+1)]*nums[i+3*(n+1)];
 		if(up > p) p = up;
-		if(dwn > p) p = dwn;
-		if(l > p) p = l;
 		if(r > p) p = r;
 		if(ur > p) p = ur;
-		if(ul > p) p = ul;
 		if(dr > p) p = dr;
-		if(dl > p) p = dl;
 	}
 	return p;
 }
