a0e78df2ed3883dd9f4d6101159c77af2d0d2484
Thomas Shields <me@thomasshields.net>
2012-03-12 16:37:44 -0400

    10

--- problem10.js
+++ problem10.js
@@ -1,46 +1,24 @@
-//Alternative (and hopefully faster)
-//using Eratosthenes's Sieve
-//This is not working yet!!
 function sum(n) {
-	var composites = [], primes=[2];
-	function filter(p) {
-		console.log(primes);
+	var i=p=2, list=[], cont=true;
+	for(;i<n;i++) { list.push(i); }
+	while(cont) {
 		var i = 2*p;
-		for(;i<n;i+=p) {
-			composites.push(i);
-			var k = primes.indexOf(i);
-			console.log(i + " removed from primes");
-			primes.splice(k,1);
+		for(;i<=list.length+1;i+=p) {
+			list[i-2] = -1;
 		}
-		var t = p+1, cont = false;
-		for(;t<n;t++) {
-			console.log(composites,t);
-			if(composites.indexOf(t) === -1) {
-				p = t;
-				primes.push(p);
+		var k = p+1;
+		cont=false;
+		for(;k<list.length+1;k++) {
+			if(list[k-2] !== -1) {
+				p=k;
 				cont = true;
 				break;
 			}
 		}
-		if(cont) filter(p);
-		else return;	
-	};
-	filter(2);
-	var i=2,
-	s=0;
-	/*for(;i<n;i++) {
-		if(composites.indexOf(i) === -1) {
-			//push to primes array if
-			//you're finding the primes
-			//since we're just finding 
-			//the sum here, i'll skip that
-			s+=i;
-		}		
-	}*/
-	var j = 0;
-	for(;j<primes.length;j++) { 
-		s+= primes[j]; 
-		console.log(primes[j] + " added to sum (which is now " + s + ")");
+	}
+	var j=s=0;
+	for(;j<list.length;j++) {
+		if(list[j] !== -1) s+= list[j];
 	}
 	return s;
-}	
+}
