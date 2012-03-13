6907ecaed54f98bdd02062f8a221358f3c2a93e0
Thomas Shields <me@thomasshields.net>
2012-03-12 23:19:17 -0400

    11 formatting; 12

--- problem11.js
+++ problem11.js
@@ -9,14 +9,16 @@ function solve(grid) {
 	var i=p=0, nums=grid.split(" "), n;
 	n = Math.sqrt(nums.length);
 	for(;i<nums.length;i++) {
-		var r = nums[i]*nums[i+1]*nums[i+2]*nums[i+3],
-		up = nums[i]*nums[i-n]*nums[i-2*n]*nums[i-3*n],
-		ur = nums[i]*nums[i-(n-1)]*nums[i-2*(n-1)]*nums[i-3*(n-1)],
-		dr = nums[i]*nums[i+(n+1)]*nums[i+2*(n+1)]*nums[i+3*(n+1)];
-		if(up > p) p = up;
-		if(r > p) p = r;
-		if(ur > p) p = ur;
-		if(dr > p) p = dr;
+		//find product of num and nums to right
+		var east = nums[i] * nums[i+1] * nums[i+2] * nums[i+3],
+		//find product of num and nums above
+		north = nums[i] * nums[i-n] * nums[i-2*n] * nums[i-3*n],
+		northeast = nums[i] * nums[i-(n-1)] * nums[i-2*(n-1)] * nums[i-3*(n-1)],
+		southeast = nums[i] * nums[i+(n+1)] * nums[i+2*(n+1)] * nums[i+3*(n+1)];
+		if(north > p) p = north;
+		if(east > p) p = east;
+		if(northeast > p) p = northeast;
+		if(southeast > p) p = southeast;
 	}
 	return p;
 }
---
+++ problem12.js
@@ -0,0 +1,36 @@
+//OBJECTIVE:
+//Find the first triangle number
+//to have over n factors
+function triangle(factors) {
+	var i=t=1,j,f,result;
+	while(!result) {
+		j=1;
+		f=0;
+		//since a triangle number is
+		//an integer, the largest factor
+		//other than itself that it
+		//will have is t/2
+		var k = t/2;
+		for(;j<k;j++) {
+			//if j is a factor of t
+			if(t%j === 0) {
+				//increment the amount
+				//of factors found sofar
+				//by two - once for j
+				//and once for t/j
+				f+=2;
+				//since we've already found
+				//the factor t/j, we can
+				//optimize the loop by
+				//lowering the limit to t/j
+				//so we don't find t/j
+				//again and count it twice
+				k=t/j;
+			}
+		}
+		if(f>factors) result = t;
+		//get the next triangle number
+		else t += ++i; 
+	}
+	return result;
+}
